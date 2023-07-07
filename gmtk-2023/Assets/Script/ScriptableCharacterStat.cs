d25424722909d79e99b80731d21752c44dec09b2
AlexBuj <ALBUJ7@ulaval.ca>
2023-07-07 15:06:55 -0400

    Added Basic ScriptableObject

---
+++ gmtk-2023/Assets/Script/ScriptableCharacterStat.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableCharacterStat : ScriptableObject
+{
+    [SerializeField]
+    private Sprite baseSprite;
+
+    [SerializeField]
+    private int _life;
+
+    public int getHealth()
+    {
+        return _life;
+    }
+    public void setHealth(int life)
+    {
+        _life = life;
+    }
+
+}
