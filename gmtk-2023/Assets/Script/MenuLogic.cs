9ec15b54ceeea7bd7b5f9110517389560638e623
Alexandre <ALBUJ7@ulaval.ca>
2023-07-08 11:54:48 -0400

    add changing text to button

--- gmtk-2023/Assets/Script/MenuLogic.cs
+++ gmtk-2023/Assets/Script/MenuLogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuLogic : MonoBehaviour
 {
@@ -34,6 +35,7 @@ public class MenuLogic : MonoBehaviour
         if (attackOptionNb == 1)
         {
             GameObject button = Instantiate(Button, gameObject.transform);
+            button.GetComponentInChildren<Text>().text = entity.Stats.Attacks[0].ToString();
             RectTransform rt = button.GetComponent<RectTransform>();
             rt.anchoredPosition = new Vector2((width / 2)-(rt.sizeDelta.x/2), (height / 2)- (rt.sizeDelta.y / 2));
         }
@@ -42,6 +44,7 @@ public class MenuLogic : MonoBehaviour
             for (int i = 0; i < attackOptionNb; i++)
             {
                 GameObject button = Instantiate(Button, gameObject.transform);
+                button.GetComponentInChildren<Text>().text = entity.Stats.Attacks[i].ToString();
                 RectTransform rt = button.GetComponent<RectTransform>();
                 rt.anchoredPosition = new Vector2(((width / 3)*(i+1)) - (rt.sizeDelta.x / 2), (height / 2) - (rt.sizeDelta.y / 2));
             }
@@ -51,10 +54,12 @@ public class MenuLogic : MonoBehaviour
             for (int i =0; i<2; i++)
             {
                 GameObject button = Instantiate(Button, gameObject.transform);
+                button.GetComponentInChildren<Text>().text = entity.Stats.Attacks[i].ToString();
                 RectTransform rt = button.GetComponent<RectTransform>();
                 rt.anchoredPosition = new Vector2((width / 3) * (i + 1) - (rt.sizeDelta.x / 2), height / 1.5f - (rt.sizeDelta.y / 2));
             }
             GameObject buttonBottom = Instantiate(Button, gameObject.transform);
+            buttonBottom.GetComponentInChildren<Text>().text = entity.Stats.Attacks[2].ToString();
             RectTransform rtBottom = buttonBottom.GetComponent<RectTransform>();
             rtBottom.anchoredPosition = new Vector2((width / 2) - (rtBottom.sizeDelta.x / 2), (height / 3f) - (rtBottom.sizeDelta.y / 2));
         }
@@ -63,12 +68,14 @@ public class MenuLogic : MonoBehaviour
             for (int i = 0; i < 2; i++)
             {
                 GameObject button = Instantiate(Button, gameObject.transform);
+                button.GetComponentInChildren<Text>().text = entity.Stats.Attacks[i].ToString();
                 RectTransform rt = button.GetComponent<RectTransform>();
                 rt.anchoredPosition = new Vector2((width / 3) * (i + 1) - (rt.sizeDelta.x / 2), height / 1.5f - (rt.sizeDelta.y / 2));
             }
             for (int i = 0; i < 2; i++)
             {
                 GameObject button = Instantiate(Button, gameObject.transform);
+                button.GetComponentInChildren<Text>().text = entity.Stats.Attacks[i].ToString();
                 RectTransform rt = button.GetComponent<RectTransform>();
                 rt.anchoredPosition = new Vector2((width / 3) * (i + 1) - (rt.sizeDelta.x / 2), height / 3.6f - (rt.sizeDelta.y / 2));
             }
